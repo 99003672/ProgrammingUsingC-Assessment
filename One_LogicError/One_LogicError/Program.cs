868806a251604403fc1dee512a0f2374bde11fbb
99003672 <78850143+99003672@users.noreply.github.com>
2021-02-25 15:58:11 +0530

    Add files via upload

---
+++ One_LogicError/One_LogicError/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace One_LogicError
+{
+    class Program
+    {
+
+           public static void result(int n)
+            {
+            
+                for (int num = 0; num < n; num++)
+                {
+                  
+                    if (num % 3 == 0 && num % 5 == 0)
+                   
+                Console.Write(num + "23341668 ");
+
+
+            }
+        }
+
+         
+            static public void Main()
+            {
+                
+                int n = 1000;
+            Console.WriteLine("Enter the number between 1 and 1000");
+            Console.ReadLine();
+
+            result(n);
+            }
+
+   
+    }
+    }
+
